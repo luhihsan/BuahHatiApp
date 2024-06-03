fc007ebdf833a3ad7c79ac58515b6e362de6bb36
wrx_365 <93203737+luhihsan@users.noreply.github.com>
2024-06-04 00:01:33 +0700

    Add OnBoarding and save state login

--- app/src/main/java/com/modul/buahhati/view/main/MainActivity.kt
+++ app/src/main/java/com/modul/buahhati/view/main/MainActivity.kt
@@ -1,15 +1,44 @@
 package com.modul.buahhati.view.main
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.modul.buahhati.R
+import com.modul.buahhati.databinding.ActivityMainBinding
+import com.modul.buahhati.view.fragment.fragment_home.HomeFragment
+import com.modul.buahhati.view.fragment.fragment_home.InputPertumbuhanFragment
+import com.modul.buahhati.view.fragment.fragment_home.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigation.setOnClickListener{
+            when(it.id){
+                R.id.menu_1 -> replaceFragment(HomeFragment())
+                R.id.menu_2 -> replaceFragment(InputPertumbuhanFragment())
+                R.id.menu_3 -> replaceFragment(ProfileFragment())
+                else -> {
+
+                }
+            }
+            true
+        }
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).itemActiveIndicatorColor = getColorStateList(R.color.pink)
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.layout_frame, fragment)
+        fragmentTransaction.commit()
     }
 }
--- app/src/main/java/com/modul/buahhati/view/splash/SplashActivity.kt
+++ app/src/main/java/com/modul/buahhati/view/splash/SplashActivity.kt
@@ -1,28 +1,57 @@
 package com.modul.buahhati.view.splash
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.view.WindowInsets
-import android.view.WindowManager
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.modul.buahhati.data.remote.LoginPreference
+import com.modul.buahhati.data.remote.dataStore
+import com.modul.buahhati.view.main.MainActivity
+import com.modul.buahhati.view.onboarding.OnBoardingActivity
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import com.modul.buahhati.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setupView()
+        setupAction()
     }
 
     private fun setupView(){
-        @Suppress("Deprecation")
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-        } else {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
+        setContentView(R.layout.activity_splash)
+        val content = findViewById<View>(android.R.id.content)
+        @Suppress("UNUSED_EXPRESSION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            content.viewTreeObserver.addOnDrawListener { false }
         }
-        supportActionBar?.hide()
+    }
+
+    private fun setupAction(){
+        val content = findViewById<View>(android.R.id.content)
+        @Suppress("UNUSED_EXPRESSION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            content.viewTreeObserver.addOnDrawListener { false }
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val login  = runBlocking {
+                LoginPreference.getInstance(this@SplashActivity.dataStore).getLoginStatus().first()
+            }
+            if(login==true){
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                finish()
+            }else{
+                startActivity(Intent(applicationContext, OnBoardingActivity::class.java))
+                finish()
+            }
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        },2000L)
+
     }
 }
